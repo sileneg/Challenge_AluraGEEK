6dd9b4d154470b07f4a5231e07ec7a67d0116135
sileneg <silenegonzalezquiroz@gmail.com>
2024-11-20 22:34:23 -0500

    Primer commit del proyecto Alura Geek

---
+++ js/api.js
@@ -0,0 +1,60 @@
+const apiURL = "http://localhost:3001/productos";
+
+// Método GET: Obtener todos los productos
+export async function obtenerProductos() {
+    try {
+        const response = await fetch(apiURL);
+        return await response.json();
+    } catch (error) {
+        console.error("Error al obtener productos:", error);
+    }
+}
+
+// Método POST: Crear un nuevo producto
+export async function agregarProducto(datosProducto) {
+    try {
+        const response = await fetch(apiURL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(datosProducto)
+        });
+        return await response.json();
+    } catch (error) {
+        console.error("Error al agregar producto:", error);
+    }
+}
+
+// Método DELETE: Eliminar un producto
+export async function eliminarProducto(id) {
+    try {
+        const response = await fetch(`${apiURL}/${id}`, {
+            method: "DELETE"
+        });
+        if (!response.ok) {
+            console.error("Error al eliminar producto. Estado:", response.status);
+            return false;
+        }
+        return true;
+    } catch (error) {
+        console.error("Error al eliminar producto:", error);
+        return false;
+    }
+}
+
+// Método PUT: Actualizar un producto
+export async function actualizarProducto(id, producto) {
+    try {
+        const response = await fetch(`${apiURL}/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(producto)
+        });
+        return await response.json();
+    } catch (error) {
+        console.error("Error al actualizar producto:", error);
+    }
+}
